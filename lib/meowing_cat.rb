7aff69a4ee8bb97715c46d385e9175176587c72b
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-09 12:18:07 -0600

    commit tree 7e2596101c6c937837a8bb7df64c4cb5e8c84e29

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
+
+whiskers = Cat.new
+whiskers.name = "Whiskers"
+puts whiskers.name
+whiskers.meow
